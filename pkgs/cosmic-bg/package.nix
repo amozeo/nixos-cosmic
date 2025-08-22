56ae2a38ad98d0febd0a02b94fd7cc55e14a0947
Wroclaw <wroclaw223@outlook.com>
2025-08-23 00:29:35 +0200

    cosmic-bg: 1.0.0-alpha.7-unstable-2025-05-01 -> 1.0.0-alpha.7-unstable-2025-08-18
    Diff: https://github.com/pop-os/cosmic-bg/compare/1da843a63656cf58b373a4823c15326be448b24e...d46d05e159a22a30713f5f49f1a79ec9a2630d96

--- pkgs/cosmic-bg/package.nix
+++ pkgs/cosmic-bg/package.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-bg";
-  version = "1.0.0-alpha.7-unstable-2025-05-01";
+  version = "1.0.0-alpha.7-unstable-2025-08-18";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-bg";
-    rev = "1da843a63656cf58b373a4823c15326be448b24e";
-    hash = "sha256-x/nCEiE+tGAlgAOJKT+zpi3fMJt9cTx0mFteibdC9FE=";
+    rev = "d46d05e159a22a30713f5f49f1a79ec9a2630d96";
+    hash = "sha256-z7PtP5sDj4Vb74eNeEDcXgoRRKcVT17Dlvx9XHX/9+4=";
   };
 
-  cargoHash = "sha256-GLXooTjcGq4MsBNnlpHBBUJGNs5UjKMQJGJuj9UO2wk=";
+  cargoHash = "sha256-iCQjPZH3CN73R6PmFRndLcPZGQfxeaPSYPZgbGofKkM=";
 
   nativeBuildInputs = [
     libcosmicAppHook
