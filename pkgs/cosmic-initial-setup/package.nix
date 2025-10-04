74bbc40b39f8afdfad4134ec841d8ee95e59e34c
Wroclaw <wroclaw223@outlook.com>
2025-10-04 09:52:04 +0200

    pkgs/cosmic-initial-setup: init at 1.0.0-beta.1.1-unstable-2025-10-01

---
+++ pkgs/cosmic-initial-setup/package.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  libcosmicAppHook,
+  libinput,
+  pkg-config,
+  udev,
+  openssl,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "cosmic-initial-setup";
+  version = "1.0.0-beta.1.1-unstable-2025-10-01";
+
+  src = fetchFromGitHub {
+    owner = "pop-os";
+    repo = "cosmic-initial-setup";
+    rev = "bd2c975e7075359a584abcc9c1220ac351824d80";
+    hash = "sha256-NR5jUD54JOMLhYffdAi2MjhcaY79TGHF4tKIcBKPhr0=";
+  };
+
+  cargoHash = "sha256-orwK9gcFXK4/+sfwRubcz0PP6YAFqsENRHnlSLttLxM=";
+
+  buildFeatures = [ "nixos" ];
+
+  auditable = false;
+
+  nativeBuildInputs = [
+    libcosmicAppHook
+    pkg-config
+  ];
+  buildInputs = [
+    openssl
+    libinput
+    udev
+  ];
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version-regex"
+      "epoch-(.*)"
+    ];
+  };
+
+  meta = {
+    description = "COSMIC Initial Setup";
+    homepage = "https://github.com/pop-os/cosmic-initial-setup";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "cosmic-initial-setup";
+    platforms = lib.platforms.linux;
+  };
+}
