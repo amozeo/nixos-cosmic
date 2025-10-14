bc5fa63e4d6f5f3bad40164028e9699c3b6bde16
Wroclaw <wroclaw223@outlook.com>
2025-10-14 21:40:30 +0200

    pkgs/cosmic-settings: fix justFlags
    `bin-src` variable was removed upstream.

--- pkgs/cosmic-settings/package.nix
+++ pkgs/cosmic-settings/package.nix
@@ -65,8 +65,8 @@ rustPlatform.buildRustPackage {
     "prefix"
     (placeholder "out")
     "--set"
-    "bin-src"
-    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-settings"
+    "cargo-target-dir"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}"
   ];
 
   postInstall = ''
