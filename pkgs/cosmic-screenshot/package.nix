a59e499b75a0264ccd7fefb77fc982d5fc42748f
Wroclaw <wroclaw223@outlook.com>
2026-05-16 12:24:56 +0200

    cosmic-screenshot: 1.0.12-unstable-2026-05-12 -> 1.0.13-unstable-2026-05-12
    Diff: https://github.com/pop-os/cosmic-screenshot/compare/b917c631d155d71374fe2f2c0cbd2f4a33326420...b917c631d155d71374fe2f2c0cbd2f4a33326420

--- pkgs/cosmic-screenshot/package.nix
+++ pkgs/cosmic-screenshot/package.nix
@@ -9,7 +9,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-screenshot";
-  version = "1.0.12-unstable-2026-05-12";
+  version = "1.0.13-unstable-2026-05-12";
 
   src = fetchFromGitHub {
     owner = "pop-os";
