4c92b43886480cc7a8eadef538d29b95b98d167c
Wroclaw <wroclaw223@outlook.com>
2026-06-06 12:12:01 +0200

    cosmic-greeter: 1.0.14-unstable-2026-05-26 -> 1.0.15-unstable-2026-05-26
    Diff: https://github.com/pop-os/cosmic-greeter/compare/178d5222ee580377f91b25e4e01ed98597d39664...178d5222ee580377f91b25e4e01ed98597d39664

--- pkgs/cosmic-greeter/package.nix
+++ pkgs/cosmic-greeter/package.nix
@@ -16,7 +16,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cosmic-greeter";
-  version = "1.0.14-unstable-2026-05-26";
+  version = "1.0.15-unstable-2026-05-26";
 
   src = fetchFromGitHub {
     owner = "pop-os";
