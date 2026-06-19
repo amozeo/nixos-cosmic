77a1a381b9ffb569f65984f0e9dfa61c1ca90738
Wroclaw <wroclaw223@outlook.com>
2026-06-19 21:00:05 +0200

    nixos: add polkit module to be repleaced

--- nixos/default.nix
+++ nixos/default.nix
@@ -12,18 +12,23 @@
 }:
 
 let
-  modules = basePath: lib.optionals (lib.versionOlder lib.trivial.version "25.11") [
-    # cosmic modules in unstable nixpkgs depends on changes to geogclue that are not on stable
-    (basePath + "/services/desktops/geoclue2.nix")
+  unstableModulesPath = nixpkgsPath + "/nixos/modules";
 
+  modules = basePath: lib.optionals (lib.versionOlder lib.trivial.version "25.11") [
+    # depends on changes to geoclue not existing on 25.05
+    (basePath + "/services/desktops/geoclue2.nix")
+  ] ++ lib.optionals (lib.versionOlder lib.trivial.version "26.11") [
+    # depends on changes to polkit not existing on 26.05
+    (basePath + "/security/polkit.nix")
+  ] ++ [
     (basePath + "/services/display-managers/cosmic-greeter.nix")
     (basePath + "/services/desktop-managers/cosmic.nix")
   ];
 in
 {
-  imports = modules (nixpkgsPath + "/nixos/modules");
+  imports = modules unstableModulesPath;
 
-  disabledModules = modules modulesPath;
+  disabledModules = lib.optionals (modulesPath != unstableModulesPath) (modules modulesPath);
 
   nixpkgs.overlays = [ cosmicOverlay ];
 }
